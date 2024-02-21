cd0cac1066d1a61e0a05edf9cc069508dd1a6135
quaint <alexgomez2797@gmail.com>
2024-02-21 12:32:24 -0500

    Completed the Pages

---
+++ src/components/BlockQuote.tsx
@@ -0,0 +1,21 @@
+import "../css/BlockQuotes.scss";
+
+interface BlockQuoteProps {
+  header: string;
+  content: string;
+  source: string;
+}
+
+function BlockQuote({ header, content, source }: BlockQuoteProps) {
+  return (
+    <blockquote className="blockquote">
+      <h3>{header}</h3>
+      <p>{content}</p>
+      <footer>
+        <cite>{source}</cite>
+      </footer>
+    </blockquote>
+  );
+}
+
+export default BlockQuote;
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,26 +1,54 @@
-import "../css/Page.scss";
-import Sidebar from "./Sidebar";
 import { Grid } from "@mui/material";
+import Sidebar from "./Sidebar";
+import Carousel from "./Carousel";
+import Accordion from "./Accordion";
+import BlockQuote from "./BlockQuote";
+import "../css/Page.scss";
 
 function Page() {
   return (
-    <>
-      <Grid
-        container
-        id="page"
-        rowSpacing={1}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      >
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Sidebar items={[{ title: "Home", href: "/" }]} />
-        </Grid>
+    <Grid
+      container
+      id="page"
+      rowSpacing={1}
+      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+    >
+      <Grid item xs={12} sm={3} md={3} lg={2} className="left-container">
+        <Sidebar items={[{ title: "Home", href: "/" }]} />
+      </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <h1>Page</h1>
-          <p>This is a page.</p>
+      {/* Use the class names from Page.scss */}
+      <Grid
+        item
+        xs={12}
+        sm={9}
+        md={9}
+        lg={10}
+        container
+        direction="column"
+        className="right-container"
+      >
+        <Grid item className="item-spacing">
+          <BlockQuote
+            header="CPR"
+            content="What it is."
+            source="Source of the CPR"
+          />
+        </Grid>
+        <Grid item className="item-spacing">
+          <Carousel
+            images={[
+              { src: "https://via.placeholder.com/800x400?text=1", alt: "1" },
+              { src: "https://via.placeholder.com/800x400?text=2", alt: "2" },
+              { src: "https://via.placeholder.com/800x400?text=3", alt: "3" },
+            ]}
+          />
+        </Grid>
+        <Grid item>
+          <Accordion />
         </Grid>
       </Grid>
-    </>
+    </Grid>
   );
 }
 
